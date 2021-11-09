9bf646f1cb3d1d122708ed0a0e408c9263a9546d
Juan Cañiza <juan@softwarenatura.com>
2021-11-08 21:51:11 -0300

    Muchos incrementos, actualmente pasa hasta el 99 a Romano

--- src/main/java/com/juancanhiza/numerosromanos/NumerosRomanos.java
+++ src/main/java/com/juancanhiza/numerosromanos/NumerosRomanos.java
@@ -4,6 +4,18 @@ public class NumerosRomanos {
 
     public String convertir(int numeroNatural) {
 
+        char[] numeros = String.valueOf(numeroNatural).toCharArray();
+
+        if (numeros.length == 1) {
+            return pasarUnidades(numeroNatural);
+        } else {
+            int unidad = Character.getNumericValue(numeros[1]);
+            int decena = Character.getNumericValue(numeros[0]);
+            return pasarDecenas(decena) + pasarUnidades(unidad);
+        }
+    }
+
+    private String pasarUnidades(int numeroNatural) {
         switch (numeroNatural){
             case 4:
                 return "IV";
@@ -11,15 +23,37 @@ public class NumerosRomanos {
                 return "IX";
         }
 
-        if (numeroNatural <= 3){
+        if (numeroNatural <= 3)
             return concatenarUnidadesRomanas(1, numeroNatural, "");
-        }
-
-        if (numeroNatural <= 8){
+        if (numeroNatural <= 8)
             return concatenarUnidadesRomanas(6, numeroNatural, "V");
-        }
 
         return null;
+    }
+
+    private String pasarDecenas(int numeroNatural) {
+        switch (numeroNatural) {
+            case 1:
+                return "X";
+            case 2:
+                return "XX";
+            case 3:
+                return "XXX";
+            case 4:
+                return "XL";
+            case 5:
+                return "L";
+            case 6:
+                return "LX";
+            case 7:
+                return "LXX";
+            case 8:
+                return "LXXX";
+            case 9:
+                return "XC";
+            default:
+                return null;
+        }
     }
 
     private String concatenarUnidadesRomanas(int inicio, int numeroNatural, String inicioRomano){
--- src/test/java/com/juancanhiza/numerosromanos/NumerosRomanosTest.java
+++ src/test/java/com/juancanhiza/numerosromanos/NumerosRomanosTest.java
@@ -3,6 +3,10 @@ package com.juancanhiza.numerosromanos;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class NumerosRomanosTest {
 
     private NumerosRomanos numerosRomanos = new NumerosRomanos();
@@ -10,55 +14,195 @@ public class NumerosRomanosTest {
     @Test
     public void unoANumeroRomano(){
         String unoEnRomano = numerosRomanos.convertir(1);
-        Assert.assertEquals(unoEnRomano, "I");
+        Assert.assertEquals("I", unoEnRomano);
     }
 
     @Test
     public void dosANumeroRomano(){
         String dosEnRomano = numerosRomanos.convertir(2);
-        Assert.assertEquals(dosEnRomano, "II");
+        Assert.assertEquals("II", dosEnRomano);
     }
 
     @Test
     public void tresANumeroRomano(){
         String tresEnRomano = numerosRomanos.convertir(3);
-        Assert.assertEquals(tresEnRomano, "III");
+        Assert.assertEquals("III", tresEnRomano);
     }
 
     @Test
     public void cuatroANumeroRomano(){
         String cuatroEnRomano = numerosRomanos.convertir(4);
-        Assert.assertEquals(cuatroEnRomano, "IV");
+        Assert.assertEquals("IV", cuatroEnRomano);
     }
 
     @Test
     public void cincoANumeroRomano(){
         String cincoEnRomano = numerosRomanos.convertir(5);
-        Assert.assertEquals(cincoEnRomano, "V");
+        Assert.assertEquals("V", cincoEnRomano);
     }
 
     @Test
     public void seisANumeroRomano(){
         String seisEnRomano = numerosRomanos.convertir(6);
-        Assert.assertEquals(seisEnRomano, "VI");
+        Assert.assertEquals("VI", seisEnRomano);
     }
 
     @Test
     public void sieteANumeroRomano(){
         String sieteEnRomano = numerosRomanos.convertir(7);
-        Assert.assertEquals(sieteEnRomano, "VII");
+        Assert.assertEquals("VII", sieteEnRomano);
     }
 
     @Test
     public void ochoANumeroRomano(){
         String ochoEnRomano = numerosRomanos.convertir(8);
-        Assert.assertEquals(ochoEnRomano, "VIII");
+        Assert.assertEquals("VIII", ochoEnRomano);
     }
 
     @Test
     public void nueveANumeroRomano(){
         String nueveEnRomano = numerosRomanos.convertir(9);
-        Assert.assertEquals(nueveEnRomano, "IX");
+        Assert.assertEquals("IX", nueveEnRomano);
     }
+
+    @Test
+    public void diezANumeroRomano(){
+        String diezEnRomano = numerosRomanos.convertir(10);
+        Assert.assertEquals("X", diezEnRomano);
+    }
+
+    @Test
+    public void onceANumeroRomano(){
+        String onceEnRomano = numerosRomanos.convertir(11);
+        Assert.assertEquals("XI", onceEnRomano);
+    }
+
+    @Test
+    public void doceANumeroRomano(){
+        String doceEnRomano = numerosRomanos.convertir(12);
+        Assert.assertEquals("XII", doceEnRomano);
+    }
+
+    @Test
+    public void treceANumeroRomano(){
+        String treceEnRomano = numerosRomanos.convertir(13);
+        Assert.assertEquals("XIII", treceEnRomano);
+    }
+
+    @Test
+    public void catorceANumeroRomano(){
+        String catorceEnRomano = numerosRomanos.convertir(14);
+        Assert.assertEquals("XIV", catorceEnRomano);
+    }
+
+    @Test
+    public void quinceANumeroRomano(){
+        String quinceEnRomano = numerosRomanos.convertir(15);
+        Assert.assertEquals("XV", quinceEnRomano);
+    }
+
+    @Test
+    public void dieciseisANumeroRomano(){
+        String dieciseisEnRomano = numerosRomanos.convertir(16);
+        Assert.assertEquals("XVI", dieciseisEnRomano);
+    }
+
+    @Test
+    public void diecisieteANumeroRomano(){
+        String diecisieteEnRomano = numerosRomanos.convertir(17);
+        Assert.assertEquals("XVII", diecisieteEnRomano);
+    }
+
+    @Test
+    public void dieciochoANumeroRomano(){
+        String dieciochoEnRomano = numerosRomanos.convertir(18);
+        Assert.assertEquals("XVIII", dieciochoEnRomano);
+    }
+
+    @Test
+    public void diecinueveANumeroRomano(){
+        String diecinueveEnRomano = numerosRomanos.convertir(19);
+        Assert.assertEquals("XIX", diecinueveEnRomano);
+    }
+
+    @Test
+    public void veinteANumeroRomano(){
+        String veinteEnRomano = numerosRomanos.convertir(20);
+        Assert.assertEquals("XX", veinteEnRomano);
+    }
+
+    @Test
+    public void veintenaANumerosRomanos(){
+        List<Integer> veintena = Arrays.asList(20, 21, 22, 23, 24, 25, 26, 27, 28, 29);
+
+        List<String> veintenaRomanos = veintena.stream()
+                .map(numero -> numerosRomanos.convertir(numero))
+                .collect(Collectors.toList());
+
+        Assert.assertEquals(veintenaRomanos.get(0), "XX");
+        Assert.assertEquals(veintenaRomanos.get(1), "XXI");
+        Assert.assertEquals(veintenaRomanos.get(2), "XXII");
+        Assert.assertEquals(veintenaRomanos.get(3), "XXIII");
+        Assert.assertEquals(veintenaRomanos.get(4), "XXIV");
+        Assert.assertEquals(veintenaRomanos.get(5), "XXV");
+        Assert.assertEquals(veintenaRomanos.get(6), "XXVI");
+        Assert.assertEquals(veintenaRomanos.get(7), "XXVII");
+        Assert.assertEquals(veintenaRomanos.get(8), "XXVIII");
+        Assert.assertEquals(veintenaRomanos.get(9), "XXIX");
+    }
+
+    @Test
+    public void treintenaANumerosRomanos(){
+        List<Integer> veintena = Arrays.asList(30, 31, 32, 33, 34, 35, 36, 37, 38, 39);
+
+        List<String> veintenaRomanos = veintena.stream()
+                .map(numero -> numerosRomanos.convertir(numero))
+                .collect(Collectors.toList());
+
+        Assert.assertEquals(veintenaRomanos.get(0), "XXX");
+        Assert.assertEquals(veintenaRomanos.get(1), "XXXI");
+        Assert.assertEquals(veintenaRomanos.get(2), "XXXII");
+        Assert.assertEquals(veintenaRomanos.get(3), "XXXIII");
+        Assert.assertEquals(veintenaRomanos.get(4), "XXXIV");
+        Assert.assertEquals(veintenaRomanos.get(5), "XXXV");
+        Assert.assertEquals(veintenaRomanos.get(6), "XXXVI");
+        Assert.assertEquals(veintenaRomanos.get(7), "XXXVII");
+        Assert.assertEquals(veintenaRomanos.get(8), "XXXVIII");
+        Assert.assertEquals(veintenaRomanos.get(9), "XXXIX");
+    }
+
+    @Test
+    public void decenasANumerosRomanos(){
+        List<Integer> decenas = Arrays.asList(40, 50, 60, 70, 80, 90);
+
+        List<String> decenasRomano = decenas.stream()
+                .map(numero -> numerosRomanos.convertir(numero))
+                .collect(Collectors.toList());
+
+        Assert.assertEquals(decenasRomano.get(0), "XL");
+        Assert.assertEquals(decenasRomano.get(1), "L");
+        Assert.assertEquals(decenasRomano.get(2), "LX");
+        Assert.assertEquals(decenasRomano.get(3), "LXX");
+        Assert.assertEquals(decenasRomano.get(4), "LXXX");
+        Assert.assertEquals(decenasRomano.get(5), "XC");
+    }
+
+    @Test
+    public void numerosRandomARomano(){
+        List<Integer> decenas = Arrays.asList(69, 99, 15, 81, 14, 55);
+
+        List<String> decenasRomano = decenas.stream()
+                .map(numero -> numerosRomanos.convertir(numero))
+                .collect(Collectors.toList());
+
+        Assert.assertEquals(decenasRomano.get(0), "LXIX");
+        Assert.assertEquals(decenasRomano.get(1), "XCIX");
+        Assert.assertEquals(decenasRomano.get(2), "XV");
+        Assert.assertEquals(decenasRomano.get(3), "LXXXI");
+        Assert.assertEquals(decenasRomano.get(4), "XIV");
+        Assert.assertEquals(decenasRomano.get(5), "LV");
+    }
+
+
 
 }
